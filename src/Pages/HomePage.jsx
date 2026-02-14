e7f02d2272c03c34e938a312eaef93d580fcca47
Suhaib Khan <suhaib0900@gmail.com>
2026-02-14 09:08:23 +0000

    Home and Route Setup

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,17 +1,35 @@
+import { Link } from "react-router-dom";
+
+import HomePageImage from "../assets/Images/homePageMainImage.png";
 import HomeLayout from "../Layouts/HomeLayout";
 
 const HomePage = () => {
   return (
     <HomeLayout>
-      <div className="pt-10 text-white flex items-center justify-center gap-10 mx-16 h-[90vh]">
+      <div className="pt-10 text-white flex items-center justify-between gap-10 mx-16 h-[90vh]">
         <div className="w-1/2 space-y-7">
           <h1 className="text-5xl font-semibold">
             Find Out Best{" "}
             <span className="text-yellow-500 font-bold">Online Courses</span>
           </h1>
-          <p className="text-center text-xl text-gray-100">
+          <p className=" text-xl text-gray-100">
             We have a large library of courses. Taught by industry experts.
           </p>
+          <div className="space-x-6 flex items-center justify-start">
+            <Link to="/courses">
+              <button className="bg-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out">
+                Explore Courses
+              </button>
+            </Link>
+            <Link to="/contact">
+              <button className="bg-transparent border border-yellow-500 px-5 py-3 rounded-md font-semibold text-lg cursor-pointer hover:bg-yellow-600 transition-all ease-in-out">
+                Contact Us
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="w-1/2 flex flex-row itens-center justify-center">
+          <img src={HomePageImage} alt="HomePageImage" />
         </div>
       </div>
     </HomeLayout>
