f2aa0e505258007d8000bcbde19b883b3382c62a
Suhaib Khan <suhaib0900@gmail.com>
2026-02-20 14:36:38 +0000

    URL UPDATED

--- src/config/AxiosInstance.js
+++ src/config/AxiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  // baseURL: import.meta.env.VITE_APP_BASE_URL + "/api/v1",
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.VITE_APP_BASE_URL + "/api/v1",
+
   withCredentials: true,
 });
 export default AxiosInstance;
